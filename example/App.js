3df63bdde185176c9e4c51106205fc6fb665aa5a
Michel Couillard <mcouillard@hortau.com>
2022-03-07 08:59:32 -0500

    Update to 0.66.4

--- example/App.js
+++ example/App.js
@@ -3,21 +3,25 @@
  * https://github.com/facebook/react-native
  *
  * @format
- * @flow
+ * @flow strict-local
  */
 
 import React, {useState} from 'react';
+import type {Node} from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
   ScrollView,
-  View,
   Text,
   StatusBar,
+  StyleSheet,
+  View,
 } from 'react-native';
-import moment from 'moment'
 
-import { WheelPicker, TimePicker, DatePicker } from 'react-native-wheel-picker-android'
+import {
+  WheelPicker,
+  TimePicker,
+  DatePicker,
+} from '@hortau/react-native-wheel-picker-android';
 
 const weekdays = [
   'sunday',
@@ -28,32 +32,53 @@ const weekdays = [
   'friday',
 ];
 
-const App: () => React$Node = () => {
-  const [selectedItem, setSelectedItem] = useState(0)
-  const [time, setTime] = useState(new Date())
-  const [date, setDate] = useState(new Date())
+const App: () => Node = () => {
+  const [selectedItem, setSelectedItem] = useState(0);
+  const [time, setTime] = useState(() => new Date());
+  const [date, setDate] = useState(() => new Date());
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic">
-          <Text>{weekdays[selectedItem]}</Text>
-          <WheelPicker onItemSelected={(index) => setSelectedItem(index)} data={weekdays}/>
-          <Text>{moment(time).format('hh:mm a')}</Text>
-          <TimePicker 
-            initDate={new Date(moment().add(1, 'hour'))} 
-            onTimeSelected={(time) => {
-              setTime(time)
-              console.log('setting time', time)
-            }
-          }/>
-          <Text>{moment(date).format('DD.MM.YY hh:mm a')}</Text>
-          <DatePicker mode={'date'} onDateSelected={date => setDate(date)}/>
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          <Text style={styles.weekdays}>{weekdays[selectedItem]}</Text>
+          <WheelPicker
+            onItemSelected={index => setSelectedItem(index)}
+            data={weekdays}
+          />
+          <TimePicker
+            initPosition={0}
+            style={styles.picker}
+            onTimeSelected={selectedTime => {
+              setTime(selectedTime);
+            }}
+          />
+          <View style={styles.divider} />
+          <DatePicker
+            initPosition={0}
+            style={styles.picker}
+            mode={'date'}
+            onDateSelected={selectedDate => setDate(selectedDate)}
+          />
         </ScrollView>
       </SafeAreaView>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  divider: {
+    marginVertical: 10,
+    borderBottomColor: 'gainsboro',
+    borderBottomWidth: 1,
+  },
+  picker: {
+    width: 90,
+    height: 200,
+  },
+  weekdays: {
+    padding: 5,
+  },
+});
 
 export default App;
